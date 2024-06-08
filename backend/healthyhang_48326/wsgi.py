08cd01424712bb17ef409d2a778b72bc0b250fb6
Crowdbotics <team@crowdbotics.com>
2024-06-08 00:14:31 +0000

    Initial commit

---
+++ backend/healthyhang_48326/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for healthyhang_48326 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "healthyhang_48326.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "healthyhang-48326",
+  url: "https://healthyhang-48326.botics.co",
+  api: "https://healthyhang-48326.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
